bbcfc9efca6c20d208b5a7339b08a82165fa5091
ANDERSON COELHO SILVA JUNIOR <andersoncojr@gmail.com>
2025-10-24 01:30:01 -0300

    Adicionando projeto completo

---
+++ projeto/Home.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+
+st.sidebar.markdown("Desenvolvido por [Anderson Coelho](https://www.linkedin.com/in/anderson-coelho-42671634a/)")
+
+st.markdown("## Bem-Vindo ao Analisador de Vendas")
+
+st.divider()
+
+st.markdown(
+    '''
+    Esse projeto foi desenvoldo como projeto final do cruso ***Python para usuários de Excel***.
+    
+    Utilizamos três principais bibliotecas para o seu desenvolvimento:
+    
+    - `Pandas`: para manipulação de dados em tabela
+    - `Plotly`: para geração de gráficos
+    - `Streamlit`: para criação desse webApp interativo que você se encontra nesse momento
+    
+    Os dados utilizados foram gerados pelo script "gerador_de_vendas.py" que se encontra junto do código fonte do projeto
+    
+    Sugestões podem ser enviadas para o email andersoncoojr@gmail.com
+    '''
+    )
